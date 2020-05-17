63fd45279e2911d66b10a3973ec3d8917fd4de0a
Lucas Kahlert <lucas.kahlert@square-src.de>
2020-05-17 12:50:29 +0200

    eslint: Allow console output
    Currently, we have a lot of console output for debugging purposes which
    is also helpful in production environments. We might add more
    sophisticated mechanisms to handle console logging but for now, simply
    allow it in every environment.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,8 @@ module.exports = {
   },
   extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-console": "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     // @see https://github.com/typescript-eslint/typescript-eslint/issues/291
     "no-dupe-class-members": "off"
